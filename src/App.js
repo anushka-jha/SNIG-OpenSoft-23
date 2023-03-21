f9f784d393604bab528553f8b98d780d391f8041
Anushka Jha <75205593+anushka-jha@users.noreply.github.com>
2023-03-21 23:03:40 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Register from "./Register";
+import Login from "./Login";
+
+function App() {
+  return (
+    <>
+    <div className="App">
+      <Login />
+    </div>
+    </>
+  );
+}
+
+export default App;
