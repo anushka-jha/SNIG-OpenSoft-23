edc630d5d4f4234571cc413f30e14cf22a9a37d8
AnikaMundhada <anika.mundhada@gmail.com>
2023-03-26 19:44:26 +0530

    footer push

--- src/Pages/Footer.js
+++ src/Pages/Footer.js
@@ -1,42 +1,34 @@
 import React from 'react';
-import './Footer.css';
+import { Container, Row, Col, Nav } from 'react-bootstrap';
 
-const Footer = () => {
+function Footer() {
   return (
-    <footer>
-      
-      <div className="footer-links">
-        <h3>Quick Links</h3>
-        <ul>
-          <li><a href="#">Home</a></li>
-          <li><a href="#">About Us</a></li>
-          <li><a href="#">Services</a></li>
-          <li><a href="#">Contact Us</a></li>
-        </ul>
-      </div>
-      <div className="footer-email">
-        <h3>Subscribe to our Newsletter</h3>
-        <form>
-          <input type="email" placeholder="Enter your email" />
-          <button type="submit">Subscribe</button>
-        </form>
-      </div>
-      <div className="footer-panels">
-        <div className="panel">
-          <h3>Panel 1</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </div>
-        <div className="panel">
-          <h3>Panel 2</h3>
-          <p>Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-        </div>
-        <div className="panel">
-          <h3>Panel 3</h3>
-          <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-        </div>
-      </div>
+    <>
+    <footer className="bg-dark text-white mt-5">
+      <Container>
+        <Row>
+          <Col md={4} className="mb-3">
+            <h5 className="mb-3">Contact Us</h5>
+            <p>Email: example@example.com</p>
+          </Col>
+          <Col md={4} className="mb-3">
+            <h5 className="mb-3">Links</h5>
+            <Nav className="flex-column">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/about">About Us</Nav.Link>
+              <Nav.Link href="/services">Services</Nav.Link>
+              <Nav.Link href="/contact">Contact Us</Nav.Link>
+            </Nav>
+          </Col>
+          <Col md={4} className="mb-3">
+            <h5 className="mb-3">Follow Us</h5>
+            <p>Social media links go here</p>
+          </Col>
+        </Row>
+      </Container>
     </footer>
+    </>
   );
-}
+};
 
 export default Footer;
