2b64a474bd0c2016d1b9972bcc3ab324e7410dbe
AnikaMundhada <anika.mundhada@gmail.com>
2023-03-27 13:23:09 +0530

    mainpage

---
+++ src/Pages/mainpage.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import Footer from "./footer.js"
+
+function mainpage() {
+  return (
+    <>
+    <nav className="navbar navbar-dark bg-primary p-25">
+        <a className="navbar-brand" href="#">
+            <img src="" width="30" height="30" alt=""></img>
+        </a> 
+        <span className="navbar-text">
+            <a href='#'>LOGIN</a>
+            <a> </a>
+            <a href='#'>SIGNUP</a>
+        </span>
+    </nav>
+    <div className="card mb-3 mt-3">
+        <img className="card-img-top" src="public\main.jpg" alt="Card image cap"></img>
+    </div>
+
+    <div className="row">
+        <div className="col-sm-4">
+            <div className="card">
+                <div className="card-body">
+                    <h1 className="card-title text-center">Heading1</h1>
+                    <p className="card-text text-center">Text1</p>
+                </div>
+            </div>
+        </div>
+        <div className="col-sm-4">
+            <div className="card">
+                <div className="card-body">
+                    <h1 className="card-title text-center">Heading2</h1>
+                    <p className="card-text text-center">Text2</p>
+                </div>
+            </div>
+        </div>
+        <div className="col-sm-4">
+            <div className="card">
+                <div className="card-body">
+                    <h1 className="card-title text-center">Heading3</h1>
+                    <p className="card-text text-center">Text3</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div>
+      <Footer/>
+    </div>   
+
+
+    </>
+  )
+}
+
+export default mainpage;
