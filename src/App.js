8c94df25e4932a6f6f21a8c2c15f93c85b5a6007
Anushka <iamanushkaaashvi@gmail.com>
2023-03-31 19:14:15 +0530

    .

--- src/App.js
+++ src/App.js
@@ -7,11 +7,7 @@ import Dashboard from "./Pages/Dashboard";
 import Mainpage from "./Pages/mainpage";
 import CreateProfile from "./Pages/Create-Profile";
 import Analytics from "./Pages/Analytics";
-<<<<<<< HEAD
 import Payment from "./Pages/Payment";
-=======
-import BankForm from "./Pages/bankform";
->>>>>>> 588852ca8c9cbfc523f52d1c202ddd16c196150f
 
 function App() {
   return (
@@ -23,12 +19,8 @@ function App() {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/createprofile" element={<CreateProfile />} />
         <Route path="/analytics" element={<Analytics />} />
-<<<<<<< HEAD
         <Route path="/payment" element={<Payment />} />
 
-=======
-        <Route path="/bankform" element={<BankForm />} />
->>>>>>> 588852ca8c9cbfc523f52d1c202ddd16c196150f
       </Routes>
     </div>
   );
